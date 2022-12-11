58b2b2bbecee0c7deb50d93fb2064e261e6066cd
Anna <ogonek0918@yandex.ru>
2022-12-11 20:02:20 +0700

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход 
+//два числа (A и B) и возводит число A 
+//в натуральную степень B.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{A} в степени {B} = {GetRank(A,B)}");
+
+
+int GetRank(int num, int rank)
+{
+
+    int result = num;
+    for (int i = 2; i <= rank; i++)
+    {
+        result = result * num;
+    }
+    return result;
+}
